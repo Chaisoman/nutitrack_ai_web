f710785349154ab0b7a4af31064b3140c8ab2855
Gershom Torkuma Sylvester <91039868+Chaisoman@users.noreply.github.com>
2025-09-20 07:16:00 +0100

    config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAdeC7w_-hSSfqSAeciH4F5vlj0p6eBXRM",
+    authDomain: "nutricare-app-f2ff8.firebaseapp.com",
+    databaseURL: "https://nutricare-app-f2ff8-default-rtdb.firebaseio.com",
+    projectId: "nutricare-app-f2ff8",
+    storageBucket: "nutricare-app-f2ff8.appspot.com",
+    messagingSenderId: "333784229349",
+    appId: "1:333784229349:web:bcf4cf43c0af618018bb0c"
+};
